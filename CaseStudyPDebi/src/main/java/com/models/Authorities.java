aa72874d8b581e2c984522dd631dd928bcdb1d6f
Prashanti <Prashanti@Prashanti_PC>
2019-11-01 17:06:04 -0400

    Setup environment

---
+++ CaseStudyPDebi/src/main/java/com/models/Authorities.java
@@ -0,0 +1,28 @@
+package com.models;
+
+public class Authorities {
+
+	private String authority;
+	
+	private User user;
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+	
+}
---
+++ CaseStudyPDebi/src/main/java/com/models/Movies.java
@@ -0,0 +1,41 @@
+package com.models;
+
+public class Movies {
+
+	private int movie_id;
+	
+	private String movie_name;
+	
+	private int year;
+
+	public int getMovie_id() {
+		return movie_id;
+	}
+
+	public void setMovie_id(int movie_id) {
+		this.movie_id = movie_id;
+	}
+
+	public String getMovie_name() {
+		return movie_name;
+	}
+
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+	
+	
+	
+	
+
+}
